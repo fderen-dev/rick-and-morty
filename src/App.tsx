e0f0a2b3cda85c45b7792c78ecd72baf2d6bcaa2
Filip Derenowski <fderen.dev@gmail.com>
2022-02-07 23:11:40 +0100

    Added fluid typography basic config and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App(): JSX.Element {
-  return <div></div>;
+  return <div style={ { minHeight: '100vh', width: '100%', display: "flex", justifyContent: "center", alignItems: "center" } }><h1 style={ { color: 'white' } }>Hello world</h1></div>;
 }
 
 export default App;
