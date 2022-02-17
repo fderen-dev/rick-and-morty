76632aa40a9e360519bdc28fe00cb05eab5564d4
Filip Derenowski <fderen.dev@gmail.com>
2022-02-17 20:53:48 +0100

    Device-specific navigation WIP

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,37 @@
-import { VFC } from 'react';
+import { FC } from 'react';
+
+import { useModalContext } from 'context/ModalContext';
+import { Desktop, Mobiles } from 'utils/Breakpoints';
 
 import styles from './navbar.module.scss';
 
 interface NavbarProps {}
 
-export const Navbar: VFC<NavbarProps> = (): JSX.Element => {
-  return <div className={ styles.container }></div>
+const MobileNavigation: FC = ({ children }) => {
+  const { open, openModal, closeModal } = useModalContext();
+
+  const toggleModal = open ? closeModal : () => openModal(<nav>{ children }</nav>);
+
+  return <button onClick={ toggleModal }>{ open }</button>
+}
+
+const DesktopNavigation: FC = ({ children }) => (
+    <div className={ styles.content }>
+        <nav>{ children }</nav>
+        <div>language selector</div>
+    </div>
+)
+
+export const Navbar: FC<NavbarProps> = ({ children }): JSX.Element => {
+  return (
+      <div className={ styles.container }>
+          <div>logo</div>
+          <Mobiles>
+              <MobileNavigation>{ children }</MobileNavigation>
+          </Mobiles>
+          <Desktop>
+              <DesktopNavigation>{ children }</DesktopNavigation>
+          </Desktop>
+      </div>
+  );
 }
