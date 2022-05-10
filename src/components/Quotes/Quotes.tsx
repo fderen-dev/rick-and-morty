2adf46c261bf08e839a76bbaccf7cb4e9e29902c
Filip Derenowski <fderen.dev@gmail.com>
2022-05-10 19:27:45 +0200

    Added basic spinner

--- src/components/Quotes/Quotes.tsx
+++ src/components/Quotes/Quotes.tsx
@@ -1,12 +1,22 @@
 import { VFC } from 'react';
 
+import { Spinner } from 'components/Spinner/Spinner';
+
 import { useQuotes } from './useQuotes';
 
 export const Quotes: VFC = () => {
   const { areQuotesLoading, quote } = useQuotes();
 
   return (
-    <div style={{ color: 'white', maxWidth: '100%', whiteSpace: 'pre-wrap' }}>
+    <div
+      style={{
+        color: 'white',
+        position: 'relative',
+        whiteSpace: 'pre-wrap',
+        width: '100%'
+      }}
+    >
+      {areQuotesLoading && <Spinner maxWidth="25%" />}
       {quote && <p>{quote.text}</p>}
     </div>
   );
